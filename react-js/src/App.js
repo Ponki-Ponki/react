add710f487ea0ea404b09f5e8a86c0bff2db126c
Pinki_Ponki <vensop@gmail.com>
2022-08-09 11:00:48 +0300

    update dz 1

---
+++ react-js/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Message from './message';
+
+function App() {
+  const text = 'world';
+
+  return (
+    <Message text={text} />
+  );
+}
+
+export default App;
---
+++ react-js/src/message.js
@@ -0,0 +1,12 @@
+
+
+
+function Message(props) {
+  return (
+    <div class='hi'>
+      Привет {props.text} !
+    </div>
+  );
+}
+
+export default Message;
